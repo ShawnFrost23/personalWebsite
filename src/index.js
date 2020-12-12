bb051c411bc33d0514941e1f97d249b396f074dd
Arth Patel <arthpatelf50@gmail.com>
2020-12-12 16:29:54 +0530

    rendering router in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Router from './Router';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Router />, document.getElementById('root'));
