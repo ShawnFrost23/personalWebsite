16da904b16488a4d6fc68adefbb81139bb2ade66
Arth Patel <arthpatelf50@gmail.com>
2020-12-12 16:30:54 +0530

    created landing page for the website

---
+++ src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ParticlesCustom from '../../Components/Particles/Particles';
+import styles from './LandingPage.module.css';
+
+const LandingPage = () => (
+  <div className={styles.LandingPage}>
+    <ParticlesCustom />
+  </div>
+);
+
+export default LandingPage;
