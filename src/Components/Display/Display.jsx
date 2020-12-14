da44415bb661b3689cfbcfd675a603694320ecac
Arth Patel <arthpatelf50@gmail.com>
2020-12-14 12:40:10 +0530

    added icons to profiles

--- src/Components/Display/Display.jsx
+++ src/Components/Display/Display.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './Display.module.css'
 import Name from '../Name/Name'
 import University from '../University/University'
+import InfoIcons from '../InfoIcons/InfoIcons'
 
 function Display() {
   const education = 'University of New South Wales'
@@ -9,6 +10,7 @@ function Display() {
     <div className={styles.container}>
       <Name />
       <University />
+      <InfoIcons />
     </div>
   )
 }
