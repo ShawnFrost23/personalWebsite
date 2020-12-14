a2bb019f9f3b0256a009a08f51f825249ad1c3a0
Arth Patel <arthpatelf50@gmail.com>
2020-12-14 11:52:47 +0530

    diplay added to the landing page

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ParticlesCustom from '../../Components/Particles/Particles';
+import Display from '../../Components/Display/Display';
 import styles from './LandingPage.module.css';
 
 const LandingPage = () => (
   <div className={styles.LandingPage}>
     <ParticlesCustom />
+    <Display />
   </div>
 );
 
