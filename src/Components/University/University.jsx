5b03e016d6b6c36f61484e1edd43709fbcdc8aaa
Arth Patel <arthpatelf50@gmail.com>
2020-12-14 12:11:50 +0530

    University Component

---
+++ src/Components/University/University.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './University.module.css';
+
+function University() {
+  return (
+    <>
+      <h2>{'University of New South Wales'}</h2>
+    </>
+  )
+}
+
+export default University
