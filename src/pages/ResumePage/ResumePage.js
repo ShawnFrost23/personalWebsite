f866da6914652a6277f6e72fe6a3a1c97ed18b73
Arth Patel <arthpatelf50@gmail.com>
2020-12-14 13:43:59 +0530

    added resume page

---
+++ src/pages/ResumePage/ResumePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ParticlesCustom from '../../Components/Particles/Particles'
+
+import styles from './ResumePage.module.css'
+
+function ResumePage() {
+  return (
+    <div className={styles.ResumePage}>
+      <ParticlesCustom />
+    </div>
+  )
+}
+
+export default ResumePage
