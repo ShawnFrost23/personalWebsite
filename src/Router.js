00dc413fda050581d7e38ac1e1035ee6f7720ee4
Arth Patel <arthpatelf50@gmail.com>
2020-12-14 13:44:11 +0530

    added path for resumePage in router

--- src/Router.js
+++ src/Router.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage/LandingPage';
+import ResumePage from './pages/ResumePage/ResumePage';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={LandingPage} />
+      <Route exact path="/resume.pdf" component={ResumePage} />
     </Switch>
   </BrowserRouter>
 );
